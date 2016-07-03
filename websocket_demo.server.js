21b74f65d2e1101e0c09f5040b2ec6b0bcf13e77
Alex Karolis <alexi2@protonmail.com>
2016-07-03 19:19:15 +1000

    improve style and refactor server

--- websocket_demo.server.js
+++ websocket_demo.server.js
@@ -1,12 +1,31 @@
+
+var port = 8000;
+
+// Set modules and server
+
+const http = require('http');
+const http_serv = http.createServer(handleHTTP).listen(port);
+const ASQ = require('asynquence');
+const node_static = require('node-static');
+const static_files = new node_static.Server(__dirname);
+const io = require('socket.io').listen(http_serv);
+
+// Handle http requests
+
 function handleHTTP(req, res) {
   if (req.method === "GET") {
-    if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
+    // Handle html that has a numeric begining
+    if (req.url === '/') {
       req.addListener("end", function(){
-        req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html")
+        req.url = "home.html"
         static_files.serve(req, res);
       });
       req.resume();
     } else if (req.url === '/jquery.js') {
+      static_files.serve(req, res);
+    } else if (req.url === '/websocket_demo.js') {
+      static_files.serve(req, res);
+    } else if (req.url === '/websocket_demo.css') {
       static_files.serve(req, res);
     } else {
       res.writeHead(403);
@@ -18,7 +37,12 @@ function handleHTTP(req, res) {
   }
 }
 
+// Setup socket connection
+
 function handleIO(socket) {
+
+  // Handle disconnection otherwise log connected
+
   function disconnect() {
     console.log('client disconnected');
   }
@@ -27,9 +51,13 @@ function handleIO(socket) {
 
   socket.on('disconnect', disconnect);
 
-  socket.on('typeit', function(msg) {
-    socket.broadcast.emit('messages', msg);
-  });
+  // Receive and send messages
+
+  socket.on('msg', function(broadcast) {
+    socket.broadcast.emit('broadcast', broadcast);
+  })
+
+  // Receive and send mouse position
 
   socket.on('spy', function(x,y) {
     socket.broadcast.emit('spy', {
@@ -38,35 +66,13 @@ function handleIO(socket) {
     });
   });
 
-  socket.on('msg', function(broadcast) {
-    socket.broadcast.emit('broadcast', broadcast);
-  })
+  // Send random numbers at one second interval to all clients
 
   var intv = setInterval(function () {
-    socket.emit('hello', Math.random());
+    socket.emit('random_number', Math.random());
   }, 1000)
 }
 
-var host = "localhost";
-var port = 8006;
-
-const http = require('http');
-const http_serv = http.createServer(handleHTTP).listen(port,host);
-const ASQ = require('asynquence');
-const node_static = require('node-static');
-const static_files = new node_static.Server(__dirname);
-const io = require('socket.io').listen(http_serv);
+// Set connection
 
 io.on('connection', handleIO);
-
-//configure socket.io
-io.configure(function() {
-  io.enable('browser client minification');
-  io.enable('browser client etag');
-  io.set('log level', 1);
-  io.set('transports', [
-    'websocket',
-    'xhr-polling',
-    'jsonp-polling'
-  ]);
-});
